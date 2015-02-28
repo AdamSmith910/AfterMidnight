3c097b864f32409628c0766146d194dfa73db357
Adam Smith <adamsmith910@aol.com>
2015-02-28 10:42:52 -0700

    added first controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'sessions/create'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/show'
+
+  get 'welcome/about'
+
+  get 'welcome/contact'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
