d886bbdd0fc088c6573e992a30d266b22ee095b3
Adam Smith <adamsmith910@aol.com>
2015-03-01 14:30:26 -0700

    implemented header and footer and rooted to about

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 Rails.application.routes.draw do
   get 'sessions/create'
-
   get 'users/new'
-
   get 'users/edit'
-
   get 'users/show'
-
   get 'welcome/about'
-
   get 'welcome/contact'
 
+  root "welcome#about"
 end
