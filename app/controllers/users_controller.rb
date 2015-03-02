c403444df2dd68ec90a7bb46af8a10377b0b01dc
Adam Smith <adamsmith910@aol.com>
2015-03-01 19:56:03 -0700

    new user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,42 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "User created"
+    else
+      flash[:alert] = "Failed to create new user"
+      redirect_to new_users_path
+    end
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to user_path(@user)
+    else
+      flash.now[:error] = @user.errors.full_messages.first
+      render "edit"
+    end
   end
 
   def show
+    @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+  	params.require(:signup).permit(:email,
+                                   :password,
+                                   :password_confirmation)
   end
 end
