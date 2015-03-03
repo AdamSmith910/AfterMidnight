ef98b55561d110c3c538008560dd81f2df8ef717
Adam Smith <adamsmith910@aol.com>
2015-03-02 22:35:58 -0700

    smtp

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
 
   config.assets.js_compressor = :uglifier
   
