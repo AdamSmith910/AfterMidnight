4cae0d6fb8e9307238d6f2355268ca2bd3a72976
Adam Smith <adamsmith910@aol.com>
2015-03-04 23:23:16 -0700

    back to mandrill

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,8 @@
 ActionMailer::Base.smtp_settings = {
-    :address   => "smtp.gmail.com",
+    :address   => "smtp.mandrill.com",
     :port      => 587, # ports 587 and 2525 are also supported with STARTTLS
     :enable_starttls_auto => true, # detects and uses STARTTLS
-    :user_name => "adamsmith910@gmail.com",
-    :password  => ENV["GMAIL_PASSWORD"], # smtp password is any valid api key
-    :authentication => 'login' # Mandrill supports 'plain' or 'login'
+    :user_name => ENV["MANDRILL_USERNAME"],
+    :password  => ENV["MANDRILL_PASSWORD"], # smtp password is any valid api key
+    :authentication => 'plain' # Mandrill supports 'plain' or 'login'
   }
