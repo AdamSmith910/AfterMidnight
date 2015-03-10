1c794401266cef2f8d6658c7037ea3c3ee9b6cf6
Adam Smith <adamsmith910@aol.com>
2015-03-10 15:19:20 -0600

    search form and users table modified

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309070031) do
+ActiveRecord::Schema.define(version: 20150310211846) do
 
   create_table "results", force: :cascade do |t|
     t.string   "name"
@@ -23,8 +23,6 @@ ActiveRecord::Schema.define(version: 20150309070031) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email"
-    t.string   "password_digest"
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
     t.string   "provider"
